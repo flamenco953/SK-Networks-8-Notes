11dcce2de4e5483786b81446934bea0882031b7b
Sanghoon Lee <gcccompil3r@gmail.com>
2024-11-21 12:44:26 +0900

    feat: [이상훈] Singleton 생성 방법 [SKAI8-x]

--- python/notes/second/app/main.py
+++ python/notes/second/app/main.py
@@ -1,7 +1,16 @@
 from dice.entity.dice import Dice
+from dice.repository.dice_repository_impl import DiceRepositoryImpl
 
 print("Our Second Class")
 
 dice = Dice()
 dice.rollDice()
 dice.printResult()
+
+diceRepository = DiceRepositoryImpl.getInstance()
+print(f"문제 없이 실행 되니?: {diceRepository}")
+
+diceRepository2 = DiceRepositoryImpl.getInstance()
+print(f"diceRepository2: {diceRepository2}")
+diceRepository3 = DiceRepositoryImpl.getInstance()
+print(f"diceRepository3: {diceRepository3}")
